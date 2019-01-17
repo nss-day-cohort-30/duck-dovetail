5bcc8e8819dc7f27652a136294392d0283b88cf5
Steve Brownlee <chortlehoort@gmail.com>
2019-01-17 10:52:32 -0600

    Can now remove items from shopping cart

--- scripts/shoppingCart.js
+++ scripts/shoppingCart.js
@@ -6,21 +6,24 @@ const displayShoppingCart = () => {
 
     let grandTotal = 0
 
-    for (const product of shoppingCart) {
+    shoppingCart.forEach((product, idx) => {
+
         cartEl.innerHTML +=
         `
         <section class="shoppingCart__item">
-          <div>${product.name}</div>
-          <div>${product.price.toLocaleString("en-US", {
-              style: "currency",
-              currency: "USD"
-          })}</div>
+        <div>${product.name}</div>
+        <div>${product.price.toLocaleString("en-US", {
+            style: "currency",
+            currency: "USD"
+        })}</div>
+
+        <button id="${idx}" class="cart_removeButton">Remove</button>
         </section>
         `
 
         grandTotal += product.price
+    })
 
-    }
     cartEl.innerHTML += `
       <h3>You owe us: ${grandTotal.toLocaleString("en-US", {
         style: "currency",
@@ -28,7 +31,27 @@ const displayShoppingCart = () => {
     })}</h3>
     `
 
+    // Get a reference to all purchase buttons
+    const allRemoveButtons = document.querySelectorAll(".cart_removeButton")
+
+    // Add a click event listener to each button
+    for (const button of allRemoveButtons) {
+        button.addEventListener(
+            "click",
+            (event) => {
+                const indexToRemove = parseInt(event.target.id)
+                shoppingCart.splice(indexToRemove, 1)
+                displayShoppingCart()
+            }
+        )
+
+    }
 }
+
+
+
+
+
 
 
 
